95031bcc4f38399fcdd2021b08ddb6537b697e89
Andrea Corradini <38738063+andreacorra@users.noreply.github.com>
2019-10-16 17:43:20 +0200

    Update activity_correlation.R

--- activity/activity_correlation.R
+++ activity/activity_correlation.R
@@ -28,11 +28,25 @@ actdata <- as.data.table(
   dbGetQuery(con,"SELECT * FROM main.activity_data_animals
                   WHERE activity_validity_code = 1 AND
                   activity_sensor_mode_code = 1;"))
+actdata$unique_id <- paste(actdata$animals_id, actdata$activity_sensors_id, sep = "_")
 dbDisconnect(con)
 
 head(actdata)
 str(actdata)
 
+
+
+
+
+
+require(zoo)
+require(zoocat)
+
+actdata
+
+act1 <- zoo(actdata$act_1)
+act2 <- zoo(actdata$act_2)
+rollcor(act1, act2, width = 9, show = FALSE, method = 'kendall')
 
 
 
